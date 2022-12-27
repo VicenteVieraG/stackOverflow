c6f8ad2c43f61c3c0ce56d3a525abd843023177e
Vicente Viera <plantedcoot706@gmail.com>
2022-12-27 13:03:29 -0600

    ResizeObserver Implemented

--- src/components/doubleSideBar/DoubleSidebar.jsx
+++ src/components/doubleSideBar/DoubleSidebar.jsx
@@ -1,16 +1,26 @@
-import {useState} from 'react';
+import {useState, useEffect, createRef} from 'react';
 import {FiHome, FiDatabase, FiPlus, FiBarChart, FiUsers, FiSettings, FiHelpCircle} from "react-icons/fi";
 import SecondSideBar from './SecondSideBar';
 import "../../styles/DSB.css";
 
 const DoubleSidebar = () => {
 	const [icons, setIcons] = useState([<FiHome/>, <FiDatabase/>, <FiPlus/>, <FiBarChart/>, <FiUsers/>, <FiSettings/>, <FiHelpCircle/>]);
+	const [compRef, setCompRef] = useState(createRef());
+
+	useEffect(() => {
+		if(!compRef.current) return;
+		
+		const resizeObserver = new ResizeObserver(() => console.log("xd"));
+		resizeObserver.observe(compRef.current);
+	}, []);
 
 	const getSimpleKey = () => Math.floor(Math.random() * 100000).toString(36);
+
+	console.log("Main_Component")
 	
 	return (
 		<>
-			<div id="mainSB" className="mainSideBar">
+			<div ref={compRef} id="mainSB" className="mainSideBar">
 				{icons.map(icon => <button key={getSimpleKey()} className="btn">{icon}</button>)}
 			</div>
 			<SecondSideBar/>
