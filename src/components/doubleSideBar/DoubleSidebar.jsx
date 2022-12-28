09b96b0c8e15a46845368b8c2a3e4aed6a3994a1
Vicente Viera <plantedcoot706@gmail.com>
2022-12-28 14:46:19 -0600

    Getting Units Convertions Working

--- src/components/doubleSideBar/DoubleSidebar.jsx
+++ src/components/doubleSideBar/DoubleSidebar.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect, createRef} from 'react';
+import {useState, createRef} from 'react';
 import {FiHome, FiDatabase, FiPlus, FiBarChart, FiUsers, FiSettings, FiHelpCircle} from "react-icons/fi";
 import SecondSideBar from './SecondSideBar';
 import "../../styles/DSB.css";
@@ -7,23 +7,16 @@ const DoubleSidebar = () => {
 	const [icons, setIcons] = useState([<FiHome/>, <FiDatabase/>, <FiPlus/>, <FiBarChart/>, <FiUsers/>, <FiSettings/>, <FiHelpCircle/>]);
 	const [compRef, setCompRef] = useState(createRef());
 
-	useEffect(() => {
-		if(!compRef.current) return;
-		
-		const resizeObserver = new ResizeObserver(() => console.log("xd"));
-		resizeObserver.observe(compRef.current);
-	}, []);
-
 	const getSimpleKey = () => Math.floor(Math.random() * 100000).toString(36);
 
 	console.log("Main_Component")
-	
+
 	return (
 		<>
 			<div ref={compRef} id="mainSB" className="mainSideBar">
 				{icons.map(icon => <button key={getSimpleKey()} className="btn">{icon}</button>)}
 			</div>
-			<SecondSideBar/>
+			<SecondSideBar compRef={compRef}/>
 		</>
 	)
 }
--- src/components/doubleSideBar/SecondSideBar.jsx
+++ src/components/doubleSideBar/SecondSideBar.jsx
@@ -11,7 +11,22 @@ const reducer = (state, action) => {
 	switch(action.type){
 		case "newPosition": {
 			console.log("NEW POSITION ACTION")
-			const mainSbPosition = (document.getElementById("mainSB")).getBoundingClientRect();
+
+			//Getting the Component
+			const comp = document.getElementById("mainSB");
+			const root = document.querySelector(":root");
+
+			//Get the Component Measures and Setting CSS Varibles their Values 
+			const mainSbPosition = comp.getBoundingClientRect();
+			root.style.setProperty("--secondSB-L", mainSbPosition.left);
+			root.style.setProperty("--secondSB-R", mainSbPosition.right);
+			root.style.setProperty("--secondSB-T", mainSbPosition.top);
+			root.style.setProperty("--secondSB-B", mainSbPosition.bottom);
+
+			console.log(getComputedStyle(root).getPropertyValue("--secondSB-L"))
+			console.log(getComputedStyle(root).getPropertyValue("--secondSB-R"))
+			console.log(getComputedStyle(root).getPropertyValue("--secondSB-T"))
+			console.log(getComputedStyle(root).getPropertyValue("--secondSB-B"))
 
 			return {position: mainSbPosition};
 		};
@@ -19,35 +34,27 @@ const reducer = (state, action) => {
 	}
 };
 
-const SecondSideBar = () => {
+const SecondSideBar = ({compRef}) => {
 	const [state, dispatch] = useReducer(reducer, null, init);
-	console.log(state.position)
-	//const nextState = reducer(state);
-	//console.log(nextState)
-
-
-	//console.log((getComputedStyle(document.getElementById("mainSB")).getPropertyValue("--secondSideBarInitialPosition")))
-	//console.log(fatherPosition)
-
-	//const [mainSideBarProps, setMainSideBarProps] = useState();
-	//const [mainSideBarPosition, setMainSideBarPosition] = useState();
 
 	useEffect(() => {
-		//Get Main SideBar Props
-		dispatch({type: "newPosition"})
-		//setMainSideBarProps((document.getElementById("mainSB")).getBoundingClientRect());
+		//Exit if the Component Is not Ready
+		if(!compRef.current) return;
+
+		//Create an Observer to get the dimensions of the MainSideBar whenever these change
+		const resizeObserver = new ResizeObserver(() => dispatch({type: "newPosition"}));
+		resizeObserver.observe(compRef.current);
+
+		//CleanUp Function
+		return () => resizeObserver.disconnect();
 	}, []);
 
 	console.log("SECOND SIDEBAR LOADED")
 	return(
 		<>
-			{
-				(
-					<div className="secondSideBar">
-						OLACRAYOLA
-					</div>
-				)
-			}
+			<div className="secondSideBar">
+				OLACRAYOLA
+			</div>		
 		</>
 	)
 }
